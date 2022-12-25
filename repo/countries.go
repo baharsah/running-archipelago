b3d0f605cb1caf332a29e044e3f64b33fecf8b7f
baharsah <me@baharsah.my.id>
2022-12-26 03:59:16 +0700

    finish to sleep

--- repo/countries.go
+++ repo/countries.go
@@ -10,7 +10,7 @@ type CountryRepo interface {
 	SetCountry(models.Countries) (models.Countries, error)
 	GetCountry(id int) (models.Countries, error)
 	GetCounties() ([]models.Countries, error)
-	UpdateCountry(models.Countries) (models.Countries, error)
+	// UpdateCountry(models.Countries) (models.Countries, error)
 	DeleteCountry(models.Countries) (models.Countries, error)
 }
 
@@ -22,4 +22,23 @@ func (r *repo) SetCountry(ctry models.Countries) (models.Countries, error) {
 	err := r.db.Debug().Create(&ctry).Error
 	return ctry, err
 
+}
+
+func (r *repo) GetCounties() ([]models.Countries, error) {
+
+	var countries []models.Countries
+	err := r.db.Debug().Find(&countries).Error
+	return countries, err
+
+}
+
+func (r *repo) GetCountry(id int) (models.Countries, error) {
+	var country models.Countries
+	err := r.db.Debug().Find(&country, id).Error
+	return country, err
+}
+
+func (r *repo) DeleteCountry(ctry models.Countries) (models.Countries, error) {
+	err := r.db.Debug().Delete(&ctry).Error
+	return ctry, err
 }
--- router/router.go
+++ router/router.go
@@ -7,5 +7,6 @@ func Router(r *mux.Router) {
 	UserRoute(r)
 	TripRoute(r)
 	TrxRouter(r)
+	CtryRouter(r)
 
 }
